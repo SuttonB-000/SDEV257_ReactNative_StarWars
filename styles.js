11e73a47bc500e6654759c986728b57e36bf5717
Sutton Bonte <sttnbonte0@gmail.com>
2026-07-02 22:16:58 -0400

    made style sheet

---
+++ styles.js
@@ -0,0 +1,27 @@
+import { Platform, StyleSheet, StatusBar } from "react-native";
+expost default StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "ghostwhite",
+    justifyContent: "space-around",
+    ...Platform.select({
+      ios: { paddingTop: 20},
+      android: { paddingTop: StatusBar.currentHeight  },
+    }),
+    box: {
+      height: 100,
+      justifyContent: "center",
+      alignSelf: "stretch",
+      alignItems: "center",
+      backgroundColor: "lightgray",
+      borderWidth: 1,
+      borderStyle: "solid",
+      borderColor: "darkslategray",
+    },
+    boxText: {
+      color: "darkslategray",
+      fontWeight: "bold",
+    },
+  });
+
