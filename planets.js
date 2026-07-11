5afd2df02dfcb1dcf5c8c248308e0b33e38c04a1
Sutton Bonte <sttnbonte0@gmail.com>
2026-07-10 23:24:39 -0400

    ensure import accuracy

--- planets.js
+++ planets.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
+import { getPlanets } from "./Swapi.js";
 import styles from "./styles.js";
 
 export default function Planets() {
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    getPlanets().then((data) => {
+      setPlanets(data.results);
+    });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text> Planets </Text>
+    <View stye={styles.container}>
+      {films.map((film) => (
+        <Text key={planets.uid}> {film.properties.title} </Text>
+      ))}
     </View>
   );
 }
