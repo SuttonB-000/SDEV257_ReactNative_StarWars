96256aa5c2a3c201cf02356d1722d2f5c9471d6e
Sutton Bonte <sttnbonte0@gmail.com>
2026-07-10 22:58:32 -0400

    flesh out api call and use

--- films.js
+++ films.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
+import { getFilms } from "./Swapi.js";
 import styles from "./styles.js";
 
 export default function Films() {
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    getFilms().then((data) => {
+      setFilms(data.results);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text> Films </Text>
+      {films.map((film) => (
+        <Text key={film.uid}> {film.properties.title} </Text>
+      ))}
     </View>
   );
 }
