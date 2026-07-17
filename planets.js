6776117dc832cc0496f8e6b3d812f0d2e6cab8b2
Sutton Bonte <sttnbonte0@gmail.com>
2026-07-16 22:53:48 -0400

    finished mod 3 requirements

--- planets.js
+++ planets.js
@@ -1,25 +1,28 @@
 import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList} from "react-native";
 import { getPlanets } from "./Swapi.js";
 import styles from "./styles.js";
 
 
 // function to return planet data from SWAPI
 export default function Planets() {
-  const [planet, setPlanets] = useState([]);
+  const [planets, setPlanets] = useState([]);
 
   useEffect(() => {
     // getPlanets id is set to one for proof of concept
-    getPlanets(1).then((planet) => {
+    getPlanets().then((planets) => {
       // Veiw output of json object
-      console.log(planet);
-      setPlanets(planet.result.properties);
+      console.log(planets);
+      setPlanets(planets.results);
     });
   }, []);
 
   return (
-    <View stye={styles.container}>
-        <Text > {planet.name} </Text>
+    <View style={styles.container}>
+      <FlatList
+        data={planets}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+      />
     </View>
   );
 }
